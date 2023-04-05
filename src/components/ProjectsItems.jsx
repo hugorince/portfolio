6c1494f5c9425c7784b4ade18199f6d163bba9a8
hugorince <hugo.rince@gmail.com>
2023-04-05 10:23:42 +0200

    hover on project boxes

--- src/components/ProjectsItems.jsx
+++ src/components/ProjectsItems.jsx
@@ -5,29 +5,31 @@ export default function ProjectsItems({projectClicked, isMobile}){
     const projects = require('./ProjectsList.json');
     
     const projs = projects.map((project) => {
-        return <motion.div key={project.name} className='flex flex-col space-y-8 justify-center items-center' onClick={() => {
-            projectClicked(project.name);
-        }}>
+        return <motion.div 
+            key={project.id} 
+            className='flex flex-col space-y-8 justify-center items-center drop-shadow hover:drop-shadow-xl hover:scale-105 transition' 
+            onClick={() => {
+                projectClicked(project.name);
+            }}>
             
-        <div className="overflow-hidden cursor-pointer relative group w-48 h-48 sm:w-46 sm:w-46 border border-zinc-700">
-        {isMobile ? <div></div> : <div className="z-50 opacity-0 group-hover:opacity-100 transition duration-500 ease-in-out cursor-pointer absolute from-black/80 to-transparent bg-gradient-to-t inset-x-0 -bottom-2 pt-30 text-white flex items-end">
-                <div>
-                    <div className="transform-gpu p-4 space-y-3 group-hover:opacity-100 group-hover:translate-y-0 translate-y-4 pb-6 transform transition duration-300 ease-in-out">
-                        <div className="italic">{project.name}</div>
+                    <div className="overflow-hidden cursor-pointer relative group w-48 h-48 sm:w-46 sm:w-46 border border-zinc-700">
+                        {isMobile ? <div></div> : <div className="z-50 opacity-0 group-hover:opacity-100 transition duration-500 ease-in-out cursor-pointer absolute from-black/80 to-transparent bg-gradient-to-t inset-x-0 -bottom-2 pt-30 text-white flex items-end">
+                                <div>
+                                    <div className="transform-gpu p-4 space-y-3 group-hover:opacity-100 group-hover:translate-y-0 translate-y-4 pb-6 transform transition duration-300 ease-in-out">
+                                        <div className="italic">{project.name}</div>
+                                    </div>
+                                </div>
+                            </div>}
+                        
+                        <div >
+                            <img
+                                alt=""
+                                className="object-cover w-48 h-48 sm:w-46 sm:h-46 w-full aspect-square hover:opacity-60 transition duration-500 ease-in-out"
+                                src={project.img}
+                            />
+                        </div>
                     </div>
-                </div>
-            </div>}
-            
-            <div >
-                <img
-                    alt=""
-                    className="object-cover w-48 h-48 sm:w-46 sm:h-46 w-full aspect-square group-hover:scale-105 hover:opacity-60 transition duration-500 ease-in-out"
-                    src={project.img}
-                    
-                />
-            </div>
-            </div>
-        </motion.div>
+                </motion.div>
       });
         return (
         <>
--- src/components/QuizResult.jsx
+++ src/components/QuizResult.jsx
@@ -29,30 +29,33 @@ export default function QuizResult({quizResult, resetQuiz, resultView, isMobile}
                                     You may like this <a className='font-bold italic'>project</a> 😉
                                     </MovingText>
                                 </motion.div>
-                                <motion.div 
-                                initial={{ scale: 0, rotate: -90 }}
-                                animate={{ opacity: [0, 1], scale: [1, 1.1, 1], rotate: 0 }}
-                                transition={{ duration: 0.6 }}
-                                className="overflow-hidden  cursor-pointer relative group w-64 h-64 border border-zinc-800" onClick={()=>resultView(projects[i].name)}>
-                                    {isMobile ? <div></div> : <div className="z-50 opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out cursor-pointer absolute from-black/80 to-transparent bg-gradient-to-t inset-x-0 -bottom-2 pt-30 text-white flex items-end">
-                                        <div>
-                                            <div className="transform-gpu p-4 space-y-3 text-xl group-hover:opacity-100 group-hover:translate-y-0 translate-y-4 pb-6 transform transition duration-300 ease-in-out">
-                                                <div className="italic">{quizResult}</div>
+                                <div className='drop-shadow hover:drop-shadow-xl hover:scale-105 transition'>
+                                    <motion.div 
+                                        key={projects[i].id}
+                                        initial={{ scale: 0, rotate: -90 }}
+                                        animate={{ opacity: [0, 1], scale: [1, 1.1, 1], rotate: 0 }}
+                                        transition={{ duration: 0.6 }}
+                                        className="overflow-hidden cursor-pointer relative group w-64 h-64 border border-zinc-800" 
+                                        onClick={()=>resultView(projects[i].name)}>
+                                        {isMobile ? <div></div> : <div className="z-50 opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out cursor-pointer absolute from-black/80 to-transparent bg-gradient-to-t inset-x-0 -bottom-2 pt-30 text-white flex items-end">
+                                            <div>
+                                                <div className="transform-gpu p-4 space-y-3 text-xl group-hover:opacity-100 group-hover:translate-y-0 translate-y-4 pb-6 transform transition duration-300 ease-in-out">
+                                                    <div className="italic">{quizResult}</div>
+                                                </div>
                                             </div>
+                                        </div>}
+                                        
+                                        <div >
+                                            <img
+                                                alt=""
+                                                className="object-cover w-full aspect-square hover:opacity-60 transition duration-500 ease-in-out"
+                                                src={projects[i].img}
+                                            />
                                         </div>
-                                    </div>}
-                                    
-                                    <div >
-                                        <img
-                                            alt=""
-                                            className="object-cover w-full aspect-square group-hover:scale-105 hover:opacity-60 transition duration-500 ease-in-out"
-                                            src={projects[i].img}
-                                        />
-                                    </div>
-                                </motion.div>
+                                    </motion.div>
+                                </div>
                             </div>
                                 <div className='flex items-center justify-center mt-8'>
-                                    
                                     <QuizButtons nextQuestion={resetQuiz} content={'Skip to portfolio'} speedAnim={0.7}/>
                                 </div>
                             </div> 
